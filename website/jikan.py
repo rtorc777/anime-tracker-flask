47c3820bdc6a350c2f79308b09fc4e945429a6fc
Rico Torculas <rtorc777@gmail.com>
2023-08-02 23:37:57 -0400

    Tested Jikan API

---
+++ website/jikan.py
@@ -0,0 +1,12 @@
+from jikanpy import Jikan
+import json
+
+jikan = Jikan()
+
+search = jikan.search('anime', 'kaguya', parameters={'limit': 10})
+
+for i in range(len(search['data'])):
+    results = search['data'][i]["title"]
+    images = search['data'][i]["images"]["jpg"]["image_url"]
+
+    print(results, images)
