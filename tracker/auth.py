4c44679f937ddc19f43348b13de0625568f75cbb
Rico Torculas <rtorc777@gmail.com>
2023-08-08 23:44:30 -0400

    Set up Registration page w/ validations

--- tracker/auth.py
+++ tracker/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from tracker.forms import RegisterForm
 from tracker.models import User
 from tracker import db
@@ -27,6 +27,6 @@ def register():
     
     if form.errors != {}:
         for err_msg in form.errors.values():
-            print(f'There was an error with creating a user: {err_msg}')
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
             
     return render_template("register.html", form=form)
--- tracker/forms.py
+++ tracker/forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired, email_validator
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from tracker.models import User
 
 class RegisterForm(FlaskForm):
+    
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists! Please try a different one')
+        
+    def validate_email(self, email_to_check):
+        email = User.query.filter_by(email=email_to_check.data).first()
+        if email:
+            raise ValidationError('Email already exists! Please try a different one')
+
     username = StringField(label='Username:', validators=[Length(min=2, max=30), DataRequired()])
     email = StringField(label='Email:', validators=[Email(), DataRequired()])
     password = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
