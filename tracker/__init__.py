79793a47756c0e6818b0bb8acf7ec8336759a653
drMico <rtorc777@gmail.com>
2023-08-30 22:58:59 -0400

    changed to postgres db

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -10,9 +12,12 @@ login_manager = LoginManager(app)
 login_manager.login_view="auth.login"
 login_manager.login_message_category="info"
 
+load_dotenv()
+URI = os.getenv("URI")
+
 def create_app():
     app.config['SECRET_KEY'] = 'secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{URI}'
     db.init_app(app)
 
     from .models import User, Item, Anime_Item, Manga_Item
